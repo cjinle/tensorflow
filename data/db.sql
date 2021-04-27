fae44fd507e54b155cc88538209854ea383cc806
cjinle <cjinle@gmail.com>
2021-04-27 14:57:46 +0800

    first

---
+++ data/db.sql
@@ -0,0 +1,26 @@
+
+// 密码
+hallmm/bVU59SQsZI9RTgDd
+hallmy/ihNwniNBlfue7L65
+
+
+create database if not exists `dc_hallmm` character set utf8; 
+create database if not exists `dc_hallmm_device` character set utf8; 
+create database if not exists `dc_hallmm_member` character set utf8; 
+create database if not exists `hallmm_achieve` character set utf8; 
+create database if not exists `hallmm_cashcow` character set utf8; 
+create database if not exists `hallmm_device` character set utf8; 
+create database if not exists `hallmm_facebook` character set utf8; 
+create database if not exists `hallmm_friend` character set utf8; 
+create database if not exists `hallmm_gift` character set utf8; 
+create database if not exists `hallmm_invite_friend` character set utf8; 
+create database if not exists `hallmm_logs` character set utf8; 
+create database if not exists `hallmm_member` character set utf8; 
+create database if not exists `hallmm_member_mobile` character set utf8; 
+create database if not exists `hallmm_msg` character set utf8; 
+create database if not exists `hallmm_mstat` character set utf8; 
+create database if not exists `hallmm_oldlogs` character set utf8; 
+create database if not exists `hallmm_recent` character set utf8; 
+create database if not exists `hallmm_storage` character set utf8; 
+
+
---
+++ scripts/test.py
@@ -0,0 +1,14 @@
+# 
+# import tensorflow as tf
+import pandas as pd
+# import numpy as np
+
+
+# print(tf.__version__)
+
+data = pd.read_csv('../data/ad.csv')
+print(data.head())
+
+
+
+
